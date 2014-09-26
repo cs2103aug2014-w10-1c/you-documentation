6c909c603f62ec549f067528fa8df041b25065ec
AppVeyor Build Server <cs2103aug2014-w10-1c@appveyor.local>
2014-09-26 09:11:15 +0000

    Merge pull request #63 from angathorion/icon-nlp-interface Basic implementation of tray icon and NLP interface.
    (main@ad7a1896abf8236db9b72198b816ed19a4bec748)

--- search/variables_2.js
+++ search/variables_2.js
@@ -1,6 +1,11 @@
 var searchData=
 [
   ['task',['task',['../class_you_1_1_n_l_p_1_1_result.html#aa1bb3e28c9ac574e6c83a0dac036e2d3',1,'You::NLP::Result']]],
+  ['task_5fcolumn_5f1',['TASK_COLUMN_1',['../class_you_main_g_u_i.html#abff0bd774f9047715b1321246db5a574',1,'YouMainGUI']]],
+  ['task_5fcolumn_5f2',['TASK_COLUMN_2',['../class_you_main_g_u_i.html#a4c7f4bb2868378683cdeb3ea13dcecad',1,'YouMainGUI']]],
+  ['task_5fcolumn_5f3',['TASK_COLUMN_3',['../class_you_main_g_u_i.html#a61ad936ffda3da1a0af4ff227ff5cb75',1,'YouMainGUI']]],
+  ['task_5fcolumn_5f4',['TASK_COLUMN_4',['../class_you_main_g_u_i.html#ae5f18ebf6da2d1cff5ed1ff65f031b2d',1,'YouMainGUI']]],
+  ['task_5fcolumn_5f5',['TASK_COLUMN_5',['../class_you_main_g_u_i.html#af16d4cee1097de3fb257e98c2ba84837',1,'YouMainGUI']]],
   ['task_5fcolumn_5fcount',['TASK_COLUMN_COUNT',['../class_you_main_g_u_i.html#ad06524754d6820391b49a36d2cdc70d9',1,'YouMainGUI']]],
   ['tasklist',['taskList',['../class_you_1_1_n_l_p_1_1_controller_1_1_context.html#a43b287449ec959f0e1d61daac3b8c399',1,'You::NLP::Controller::Context::taskList()'],['../class_you_1_1_n_l_p_1_1_result.html#a11349c6ca55d941360d15325f307eee2',1,'You::NLP::Result::taskList()']]]
 ];
