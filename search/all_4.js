f8713b94548c50daadac46a9df65ee710b53a0bf
AppVeyor Build Server <cs2103aug2014-w10-1c@appveyor.local>
2014-09-27 11:13:27 +0000

    Merge pull request #75 from lowjoel/fix-doxygen-warnings Fix doxygen warnings
    (main@d5d8f4bcdadcb79017684bdbd9cad381a259ded5)

--- search/all_4.js
+++ search/all_4.js
@@ -1,8 +1,8 @@
 var searchData=
 [
   ['emptytaskdescriptionexception',['EmptyTaskDescriptionException',['../class_you_1_1_query_engine_1_1_internal_1_1_empty_task_description_exception.html',1,'You::QueryEngine::Internal']]],
-  ['exception',['Exception',['../class_you_1_1_query_engine_1_1_internal_1_1_exception.html',1,'You::QueryEngine::Internal']]],
   ['exception',['Exception',['../class_you_1_1_n_l_p_1_1_exception.html',1,'You::NLP']]],
+  ['exception',['Exception',['../class_you_1_1_query_engine_1_1_internal_1_1_exception.html',1,'You::QueryEngine::Internal']]],
   ['exception_2eh',['exception.h',['../_you-_query_engine_2internal_2exception_8h.html',1,'']]],
   ['executequery',['executeQuery',['../class_you_1_1_query_engine_1_1_query.html#ab0c291eba3380b3e3470b63bf43d923b',1,'You::QueryEngine::Query']]]
 ];
--- search/classes_4.js
+++ search/classes_4.js
@@ -1,6 +1,6 @@
 var searchData=
 [
   ['emptytaskdescriptionexception',['EmptyTaskDescriptionException',['../class_you_1_1_query_engine_1_1_internal_1_1_empty_task_description_exception.html',1,'You::QueryEngine::Internal']]],
-  ['exception',['Exception',['../class_you_1_1_n_l_p_1_1_exception.html',1,'You::NLP']]],
-  ['exception',['Exception',['../class_you_1_1_query_engine_1_1_internal_1_1_exception.html',1,'You::QueryEngine::Internal']]]
+  ['exception',['Exception',['../class_you_1_1_query_engine_1_1_internal_1_1_exception.html',1,'You::QueryEngine::Internal']]],
+  ['exception',['Exception',['../class_you_1_1_n_l_p_1_1_exception.html',1,'You::NLP']]]
 ];
