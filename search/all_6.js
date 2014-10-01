5c40590976eea7f5e355a109fc55c966e66398a7
AppVeyor Build Server <cs2103aug2014-w10-1c@appveyor.local>
2014-10-01 00:29:12 +0000

    Merge pull request #86 from lowjoel/stub-controller Stub the controller class.
    (main@34b491b52818de055aa004d61b9425caa7a04c70)

--- search/all_6.js
+++ search/all_6.js
@@ -1,5 +1,5 @@
 var searchData=
 [
-  ['get',['get',['../class_you_1_1_data_store_1_1_data_store.html#a62e786ee7cb6b7429b6ab5c63ae74051',1,'You::DataStore::DataStore::get()'],['../class_you_1_1_n_l_p_1_1_controller.html#a6dff4631b6f2abd86eff6158da3a721a',1,'You::NLP::Controller::get()'],['../class_you_1_1_query_engine_1_1_internal_1_1_task_builder.html#a2c22da8a9939b360b62a5a3ca7b9d425',1,'You::QueryEngine::Internal::TaskBuilder::get()']]],
+  ['get',['get',['../class_you_1_1_data_store_1_1_data_store.html#a62e786ee7cb6b7429b6ab5c63ae74051',1,'You::DataStore::DataStore::get()'],['../class_you_1_1_n_l_p_1_1_controller.html#abda2a7508353a4b983580ee4294fb300',1,'You::NLP::Controller::get()'],['../class_you_1_1_query_engine_1_1_internal_1_1_task_builder.html#a2c22da8a9939b360b62a5a3ca7b9d425',1,'You::QueryEngine::Internal::TaskBuilder::get()']]],
   ['gettasklist',['getTaskList',['../class_you_1_1_n_l_p_1_1_controller_1_1_context.html#a64e5ec19cc827ef2d3572db4fd8263db',1,'You::NLP::Controller::Context']]]
 ];
