a127d17505268fd8a32e5463fb9772d51cd4195c
AppVeyor Build Server <cs2103aug2014-w10-1c@appveyor.local>
2014-10-01 03:38:21 +0000

    Merge pull request #92 from lowjoel/more-controller-methods Controller methods for retrieving some subset of tasks
    (main@4957e206effac62a75343dab5ca29bc62047b068)

--- search/all_6.js
+++ search/all_6.js
@@ -1,5 +1,6 @@
 var searchData=
 [
   ['get',['get',['../class_you_1_1_data_store_1_1_data_store.html#a62e786ee7cb6b7429b6ab5c63ae74051',1,'You::DataStore::DataStore::get()'],['../class_you_1_1_n_l_p_1_1_controller.html#abda2a7508353a4b983580ee4294fb300',1,'You::NLP::Controller::get()'],['../class_you_1_1_query_engine_1_1_internal_1_1_task_builder.html#a2c22da8a9939b360b62a5a3ca7b9d425',1,'You::QueryEngine::Internal::TaskBuilder::get()']]],
-  ['gettasklist',['getTaskList',['../class_you_1_1_n_l_p_1_1_controller_1_1_context.html#a64e5ec19cc827ef2d3572db4fd8263db',1,'You::NLP::Controller::Context']]]
+  ['gettasklist',['getTaskList',['../class_you_1_1_n_l_p_1_1_controller_1_1_context.html#a64e5ec19cc827ef2d3572db4fd8263db',1,'You::NLP::Controller::Context']]],
+  ['gettasks',['getTasks',['../class_you_1_1_n_l_p_1_1_controller.html#aeca7de6eccc1ed4049eaef18145d0984',1,'You::NLP::Controller::getTasks(const std::vector&lt; Task::ID &gt; &amp;taskIDs) const '],['../class_you_1_1_n_l_p_1_1_controller.html#aed07f01011a5e3f9bd83997e746184c6',1,'You::NLP::Controller::getTasks() const ']]]
 ];
