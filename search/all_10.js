0c81e2bfab667fb7008469fc6eca083e3c036181
AppVeyor Build Server <cs2103aug2014-w10-1c@appveyor.local>
2014-10-07 01:28:18 +0000

    Merge pull request #138 from angathorion/fix-interface Shifted around addTaskListToPanel.
    (main@f8674d29fd5979e23c983fc76d2b83d9c4d350a4)

--- search/all_10.js
+++ search/all_10.js
@@ -1,7 +1,7 @@
 var searchData=
 [
   ['task',['Task',['../class_you_1_1_query_engine_1_1_task.html',1,'You::QueryEngine']]],
-  ['task',['Task',['../class_you_1_1_query_engine_1_1_task.html#a27c41adfa99810a5635302646651b5b3',1,'You::QueryEngine::Task::Task()'],['../struct_you_1_1_controller_1_1_a_d_d___r_e_s_u_l_t.html#aaac76375df4639dd247f99c95ba86497',1,'You::Controller::ADD_RESULT::task()'],['../struct_you_1_1_controller_1_1_e_d_i_t___r_e_s_u_l_t.html#ac48d2598f935a7ea0887e19e9126e6ae',1,'You::Controller::EDIT_RESULT::task()']]],
+  ['task',['task',['../struct_you_1_1_controller_1_1_a_d_d___r_e_s_u_l_t.html#aaac76375df4639dd247f99c95ba86497',1,'You::Controller::ADD_RESULT::task()'],['../struct_you_1_1_controller_1_1_e_d_i_t___r_e_s_u_l_t.html#ac48d2598f935a7ea0887e19e9126e6ae',1,'You::Controller::EDIT_RESULT::task()'],['../class_you_1_1_query_engine_1_1_task.html#a27c41adfa99810a5635302646651b5b3',1,'You::QueryEngine::Task::Task()']]],
   ['task_5fbuilder_2eh',['task_builder.h',['../task__builder_8h.html',1,'']]],
   ['task_5fcolumn_5f1',['TASK_COLUMN_1',['../class_you_main_g_u_i.html#abff0bd774f9047715b1321246db5a574',1,'YouMainGUI']]],
   ['task_5fcolumn_5f2',['TASK_COLUMN_2',['../class_you_main_g_u_i.html#a4c7f4bb2868378683cdeb3ea13dcecad',1,'YouMainGUI']]],
@@ -16,7 +16,7 @@ var searchData=
   ['taskbuildertest',['TaskBuilderTest',['../class_you_1_1_query_engine_1_1_unit_tests_1_1_task_builder_test.html',1,'You::QueryEngine::UnitTests']]],
   ['taskgraph',['TaskGraph',['../class_you_1_1_query_engine_1_1_internal_1_1_task_graph.html',1,'You::QueryEngine::Internal']]],
   ['taskid',['taskID',['../struct_you_1_1_n_l_p_1_1_d_e_l_e_t_e___q_u_e_r_y.html#a03b892dc91f8a37227cc00dab813c2ee',1,'You::NLP::DELETE_QUERY::taskID()'],['../struct_you_1_1_n_l_p_1_1_e_d_i_t___q_u_e_r_y.html#ab6d4f24aa18b8c094104993464510979',1,'You::NLP::EDIT_QUERY::taskID()']]],
-  ['taskids',['taskIDs',['../class_you_main_g_u_i_1_1_session_manager.html#a5d26071ce268824c0deb43b195fa3eee',1,'YouMainGUI::SessionManager']]],
+  ['taskids',['taskIDs',['../class_you_main_g_u_i_1_1_session_manager.html#a7094d869b14919da8884a89fa6db65f2',1,'YouMainGUI::SessionManager']]],
   ['tasklist',['TaskList',['../class_you_1_1_controller_1_1_unit_tests_1_1_mocks_1_1_task_list.html',1,'You::Controller::UnitTests::Mocks']]],
   ['tasknotfoundexception',['TaskNotFoundException',['../class_you_1_1_query_engine_1_1_internal_1_1_task_not_found_exception.html',1,'You::QueryEngine::Internal']]],
   ['taskpanelmanager',['TaskPanelManager',['../class_you_main_g_u_i_1_1_task_panel_manager.html#abeca62039696373e8c69717d526483a7',1,'YouMainGUI::TaskPanelManager']]],
