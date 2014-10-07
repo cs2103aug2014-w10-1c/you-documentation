9236114098fc6327e0e13304bd6898ac4291c708
AppVeyor Build Server <cs2103aug2014-w10-1c@appveyor.local>
2014-10-07 15:29:51 +0000

    Merge pull request #150 from evansb/implement-state Implement Marking Tasks as Completed
    (main@523c2556da8f542f0bf73501538e624b7c35174b)

--- search/all_11.js
+++ search/all_11.js
@@ -1,6 +1,6 @@
 var searchData=
 [
   ['update_5ftask_2eh',['update_task.h',['../update__task_8h.html',1,'']]],
-  ['updatetask',['updateTask',['../class_you_1_1_query_engine_1_1_internal_1_1_task_graph.html#ae9263106bc8784ae65cd8173ec0d6efd',1,'You::QueryEngine::Internal::TaskGraph::updateTask()'],['../class_you_1_1_query_engine_1_1_internal_1_1_action_1_1_update_task.html#aa1c2615894e3c7470eafeb9230fc341b',1,'You::QueryEngine::Internal::Action::UpdateTask::UpdateTask()']]],
+  ['updatetask',['updateTask',['../class_you_1_1_query_engine_1_1_internal_1_1_task_graph.html#ae9263106bc8784ae65cd8173ec0d6efd',1,'You::QueryEngine::Internal::TaskGraph::updateTask()'],['../class_you_1_1_query_engine_1_1_internal_1_1_action_1_1_update_task.html#aa1c2615894e3c7470eafeb9230fc341b',1,'You::QueryEngine::Internal::Action::UpdateTask::UpdateTask(Task::ID id, Task::Description description, Task::Time deadline, Task::Priority priority, Task::Dependencies dependencies)'],['../class_you_1_1_query_engine_1_1_internal_1_1_action_1_1_update_task.html#a867e282ab1cebcea0c02e72e66009db0',1,'You::QueryEngine::Internal::Action::UpdateTask::UpdateTask(Task::ID id, bool completed)']]],
   ['updatetask',['UpdateTask',['../class_you_1_1_query_engine_1_1_internal_1_1_action_1_1_update_task.html',1,'You::QueryEngine::Internal::Action']]]
 ];
