d80579836314d173d9068366906458a73ec97ba4
AppVeyor Build Server <cs2103aug2014-w10-1c@appveyor.local>
2014-10-12 07:34:25 +0000

    Merge pull request #182 from angathorion/exception-handling Exception handling, prettification and notification of invalid input.
    (main@ad62cfe073add54295506af7730dacf6be443ba3)

--- search/all_b.js
+++ search/all_b.js
@@ -18,5 +18,5 @@ var searchData=
   ['option_3c_20boost_3a_3aposix_5ftime_3a_3aptime_20_3e',['Option&lt; boost::posix_time::ptime &gt;',['../class_you_1_1_utils_1_1_option.html',1,'You::Utils']]],
   ['option_3c_20std_3a_3awstring_20_3e',['Option&lt; std::wstring &gt;',['../class_you_1_1_utils_1_1_option.html',1,'You::Utils']]],
   ['option_3c_20taskpriority_20_3e',['Option&lt; TaskPriority &gt;',['../class_you_1_1_utils_1_1_option.html',1,'You::Utils']]],
-  ['order',['Order',['../struct_you_1_1_n_l_p_1_1_s_h_o_w___q_u_e_r_y.html#aa1624ad34144e92b2c34d9ac70fafba3',1,'You::NLP::SHOW_QUERY::Order()'],['../struct_you_1_1_n_l_p_1_1_s_h_o_w___q_u_e_r_y_1_1_f_i_e_l_d___o_r_d_e_r.html#a21f3e5ebc1b8c9b98c82655d46cef772',1,'You::NLP::SHOW_QUERY::FIELD_ORDER::order()'],['../struct_you_1_1_n_l_p_1_1_s_h_o_w___q_u_e_r_y.html#a6638de661a65f03caf18dc7f6078e425',1,'You::NLP::SHOW_QUERY::order()']]]
+  ['order',['order',['../struct_you_1_1_n_l_p_1_1_s_h_o_w___q_u_e_r_y_1_1_f_i_e_l_d___o_r_d_e_r.html#a21f3e5ebc1b8c9b98c82655d46cef772',1,'You::NLP::SHOW_QUERY::FIELD_ORDER::order()'],['../struct_you_1_1_n_l_p_1_1_s_h_o_w___q_u_e_r_y.html#a6638de661a65f03caf18dc7f6078e425',1,'You::NLP::SHOW_QUERY::order()'],['../struct_you_1_1_n_l_p_1_1_s_h_o_w___q_u_e_r_y.html#aa1624ad34144e92b2c34d9ac70fafba3',1,'You::NLP::SHOW_QUERY::Order()']]]
 ];
