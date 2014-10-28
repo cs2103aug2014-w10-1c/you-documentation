ced7cad782256f59535de7032bbbe1ae9ca5afda
AppVeyor Build Server <cs2103aug2014-w10-1c@appveyor.local>
2014-10-28 12:11:07 +0000

    Merge pull request #261 from evansb/id-gen Store max id in DataStore. Fixes #201
    (main@99723c6f069f61ce2340ea822d11049ad14d9447)

--- search/functions_2.js
+++ search/functions_2.js
@@ -3,6 +3,7 @@ var searchData=
   ['clear',['clear',['../class_you_1_1_query_engine_1_1_internal_1_1_state.html#a6403b9e84c91bead84931fb1a1ebf8df',1,'You::QueryEngine::Internal::State']]],
   ['cleartasks',['clearTasks',['../class_you_1_1_g_u_i_1_1_main_window.html#a633663d6a448ecc6e04e17221acf2637',1,'You::GUI::MainWindow']]],
   ['commit',['commit',['../class_you_1_1_data_store_1_1_internal_1_1_transaction.html#a1da7601819d05c1d9e8acf421de1c3b0',1,'You::DataStore::Internal::Transaction::commit()'],['../class_you_1_1_data_store_1_1_transaction.html#a277f00d5732916e26233fdcf2024381c',1,'You::DataStore::Transaction::commit()']]],
+  ['commitmaxidtodatastore',['commitMaxIDToDataStore',['../class_you_1_1_query_engine_1_1_internal_1_1_state.html#a70aa302615a7bf7772da7401170259ab',1,'You::QueryEngine::Internal::State']]],
   ['comparator',['Comparator',['../class_you_1_1_query_engine_1_1_comparator.html#a1b57ef6fac28cb2c6f2d1eecd0c39125',1,'You::QueryEngine::Comparator']]],
   ['completed',['completed',['../class_you_1_1_query_engine_1_1_filter.html#a6acc39887e90f88af4f4e48578015202',1,'You::QueryEngine::Filter']]],
   ['context',['Context',['../class_you_1_1_controller_1_1_controller_1_1_context.html#aee6d560b3c1300ae02f892b05089af3c',1,'You::Controller::Controller::Context']]],
