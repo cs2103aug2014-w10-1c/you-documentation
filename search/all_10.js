c910c970bb3bb3d704ea9da5529e5a23fe7d694f
AppVeyor Build Server <cs2103aug2014-w10-1c@appveyor.local>
2014-10-28 00:38:50 +0000

    Merge pull request #252 from evansb/undo-response Refresh the current filter and comparator after undo
    (main@fff27ebce06ede3b182eb092e480ed03f1766c89)

--- search/all_10.js
+++ search/all_10.js
@@ -5,6 +5,8 @@ var searchData=
   ['serializer',['Serializer',['../class_you_1_1_query_engine_1_1_internal_1_1_controller.html#a132beef508a6d62225b1c1cc2ad0544b',1,'You::QueryEngine::Internal::Controller']]],
   ['sessionmanager',['SessionManager',['../class_you_1_1_g_u_i_1_1_main_window_1_1_session_manager.html',1,'You::GUI::MainWindow']]],
   ['sessionmanager',['SessionManager',['../class_you_1_1_g_u_i_1_1_main_window_1_1_session_manager.html#a0c8d7516fe8d4927ff34a1bb31b651a0',1,'You::GUI::MainWindow::SessionManager']]],
+  ['setactivecomparator',['setActiveComparator',['../class_you_1_1_query_engine_1_1_internal_1_1_state.html#a29d0d35b845d9e5c3530b03ce5d4fee1',1,'You::QueryEngine::Internal::State']]],
+  ['setactivefilter',['setActiveFilter',['../class_you_1_1_query_engine_1_1_internal_1_1_state.html#ac89a0cc22bd8f0479f305534c3932c2c',1,'You::QueryEngine::Internal::State']]],
   ['setloglevel',['setLogLevel',['../class_you_1_1_utils_1_1_log.html#abc139e65dcb8b92cd34b279d54387e27',1,'You::Utils::Log']]],
   ['setsink',['setSink',['../class_you_1_1_utils_1_1_log.html#af739ad468c59f3947eba570f3b2232b9',1,'You::Utils::Log']]],
   ['setup',['setup',['../class_you_1_1_g_u_i_1_1_main_window_1_1_base_manager.html#a0390168a9a7d7a623e1442c94c1d8bf1',1,'You::GUI::MainWindow::BaseManager::setup()'],['../class_you_1_1_g_u_i_1_1_main_window_1_1_query_manager.html#a86197c02fc85afdc3d675b879d79e705',1,'You::GUI::MainWindow::QueryManager::setup()'],['../class_you_1_1_g_u_i_1_1_main_window_1_1_session_manager.html#affffbf14378d143e7feec303720d17a2',1,'You::GUI::MainWindow::SessionManager::setup()'],['../class_you_1_1_g_u_i_1_1_main_window_1_1_system_tray_manager.html#af9e98e00742399193ab38c6fd239d82d',1,'You::GUI::MainWindow::SystemTrayManager::setup()'],['../class_you_1_1_g_u_i_1_1_main_window_1_1_task_panel_manager.html#a6905d71f29a35531ee807e84f2a9ac82',1,'You::GUI::MainWindow::TaskPanelManager::setup()']]],
@@ -15,6 +17,6 @@ var searchData=
   ['stringtype',['StringType',['../class_you_1_1_n_l_p_1_1_date_time_parser.html#ad44734bb65ae0695eb9df717ce8ce52c',1,'You::NLP::DateTimeParser::StringType()'],['../class_you_1_1_n_l_p_1_1_query_parser.html#a43e3e8eabdc5fbd901d3783d4f50f655',1,'You::NLP::QueryParser::StringType()']]],
   ['super_5fimportant_5ftask',['SUPER_IMPORTANT_TASK',['../namespace_you_1_1_query_engine_1_1_unit_tests.html#a6529b9b61e180d269363086205e7acb4',1,'You::QueryEngine::UnitTests']]],
   ['syntaxhighlighter',['SyntaxHighlighter',['../class_you_1_1_g_u_i_1_1_syntax_highlighter.html',1,'You::GUI']]],
-  ['systemtraymanager',['SystemTrayManager',['../class_you_1_1_g_u_i_1_1_main_window_1_1_system_tray_manager.html#aaa8f72be054fd69b0126b0b9ccdd2978',1,'You::GUI::MainWindow::SystemTrayManager']]],
-  ['systemtraymanager',['SystemTrayManager',['../class_you_1_1_g_u_i_1_1_main_window_1_1_system_tray_manager.html',1,'You::GUI::MainWindow']]]
+  ['systemtraymanager',['SystemTrayManager',['../class_you_1_1_g_u_i_1_1_main_window_1_1_system_tray_manager.html',1,'You::GUI::MainWindow']]],
+  ['systemtraymanager',['SystemTrayManager',['../class_you_1_1_g_u_i_1_1_main_window_1_1_system_tray_manager.html#aaa8f72be054fd69b0126b0b9ccdd2978',1,'You::GUI::MainWindow::SystemTrayManager']]]
 ];
