75ecfc45abb7625f2cbb94b666ec52d1296cae0d
AppVeyor Build Server <cs2103aug2014-w10-1c@appveyor.local>
2014-11-01 03:14:38 +0000

    Merge pull request #275 from digawp/wipe-ds Wipe data
    (main@aef6931275a08d0fbe74d77fac9ba3620c2ded6d)

--- search/all_14.js
+++ search/all_14.js
@@ -1,5 +1,6 @@
 var searchData=
 [
   ['warning',['warning',['../class_you_1_1_utils_1_1_log.html#afc0290f4bd0ca0a8d4bc5b002c84161b',1,'You::Utils::Log']]],
+  ['wipedata',['wipeData',['../class_you_1_1_data_store_1_1_data_store.html#a15319bfe500f6371849e07b1556f0779',1,'You::DataStore::DataStore::wipeData()'],['../class_you_1_1_data_store_1_1_internal_1_1_data_store.html#afc65554bb8ef51da08d334872b065437',1,'You::DataStore::Internal::DataStore::wipeData()']]],
   ['write',['write',['../class_you_1_1_utils_1_1_logger.html#a2a9b2329b1aa8f4a1c6fb6b5daf493bf',1,'You::Utils::Logger']]]
 ];
--- search/functions_13.js
+++ search/functions_13.js
@@ -1,4 +1,5 @@
 var searchData=
 [
+  ['wipedata',['wipeData',['../class_you_1_1_data_store_1_1_data_store.html#a15319bfe500f6371849e07b1556f0779',1,'You::DataStore::DataStore::wipeData()'],['../class_you_1_1_data_store_1_1_internal_1_1_data_store.html#afc65554bb8ef51da08d334872b065437',1,'You::DataStore::Internal::DataStore::wipeData()']]],
   ['write',['write',['../class_you_1_1_utils_1_1_logger.html#a2a9b2329b1aa8f4a1c6fb6b5daf493bf',1,'You::Utils::Logger']]]
 ];
